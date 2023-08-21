a1a3b947c1f64762b23d6b3d25c722f230a2a944
simo994q <simonkn1@hotmail.com>
2023-08-21 10:13:33 +0200

    23
    dewdef

--- react-timer/src/App.jsx
+++ react-timer/src/App.jsx
@@ -6,21 +6,39 @@ function App() {
 
   const [count, setCount] = useState(0)
 
-  
-  function startTimer() {
-    useEffect(() => {
-      setTimeout(() => {
-        setCount(() => count + 1);
-      }, 1000);
-    });
-  }
+  const [isRunning, setIsRunning] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (isRunning === true) {
+        setCount((count) => count + 1);
+      }
+    }, 1000);
+  }, [isRunning, count]);
 
 
   return (
     <>
       <h2>Tid i sekunder:</h2>
       <h3>{count}</h3>
-      <button onClick={startTimer()}>Start</button>
+      <button onClick={() => {
+        if (isRunning === false) {
+          setIsRunning(true)
+        }
+      }}>Start</button>
+      <button onClick={() => {
+        if (isRunning === true) {
+          setIsRunning(false)
+        }
+      }}>Stop</button>
+      <button onClick={() => {
+        if (isRunning === true) {
+          setIsRunning(false)
+          setCount(0)
+        } else {
+          setCount(0)
+        }
+      }}>Nulstil</button>
     </>
   )
 }
